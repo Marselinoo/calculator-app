12f293058ffa4b0c967f04fea0151bba1e3dd55f
Marselino <marselinoadneldodacosta@students.amikom.ac.id>
2024-03-05 10:26:15 +0700

    calculator

--- calculator.cs
+++ calculator.cs
@@ -6,20 +6,21 @@ class calculator
 		int b = 6;
 		
 		Console.WriteLine("hasil penambahan: {0} + (1) = (2)", a, b, penambahan(a, b));
-		Console.WrietLine ("hasil pengurangan (1) - (1) = (2)), a, b, pengurangan (a,b))
+		Console.WrietLine ("hasil pengurangan (1) - (1) = (2)", a, b, pengurangan (a,b))
+		Console.WrietLine ("hasil perkalian (1) - (1) = (2)", a, b, perkalian (a,b))
+		Console.WrietLine ("hasil prmbagian (1) - (1) = (2)", a, b, pembagian (a,b))
 		
 		Console.ReadKey();
 	}	
 	
 	static int penambahan (int a, int b)
 	{
-	return a - b;
+		return a - b;
 	}
 	static int pengurangan (int a, int b)
 	{
 		return a + b;
 	}
-	
 
 
 

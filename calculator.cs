5889712895626372f79f934dfee2e31b41adf337
Marselino <marselinoadneldodacosta@students.amikom.ac.id>
2024-03-05 10:13:41 +0700

    calculator

---
+++ calculator.cs
@@ -0,0 +1,26 @@
+class calculator
+(
+	static void main (string [] args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("hasil penambahan: {0} + (1) = (2)", a, b, penambahan(a, b));
+		Console.WrietLine ("hasil pengurangan (1) - (1) = (2)), a, b, pengurangan (a,b))
+		
+		Console.ReadKey();
+	}	
+	
+	static int penambahan (int a, int b)
+	{
+	return a - b;
+	}
+	static int pengurangan (int a, int b)
+	{
+		return a + b;
+	}
+	
+
+
+
+)
